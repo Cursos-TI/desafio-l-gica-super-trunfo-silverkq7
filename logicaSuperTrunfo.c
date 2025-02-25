a2be2664a1a8ba3bc7dcdb31f0097ce9f00af8e2
silverkq7 <vinico365@gmail.com>
2025-02-25 20:17:27 +0000

    inicio desfio Novato

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,13 +8,24 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
-
+        char Estado ='A', Estado ='B';
+        char Nome_da_CidadeA[20]="Sao Paulo",Nome_da_CidadeB[20]="Rio de janeiro";
+        char codigodacidadeA[20],codigodacidadeB[20];
+        int PopulacaoA , PopulacaoB ;
+        float AreaemKmA , AreaemKmB ;
+        float PibA , PibB ;
+        int Pontos_TuristicoA , Pontos_TuristicoB;
     
-    // Cadastro das Cartas:
+     
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
     // utilize o código do primeiro desafio
-
+    printf("Digite os dados da Cidade :\n");
+    scanf("%s",&codigodacidadeA,&codigodacidadeB);
+    scanf("%i",&PopulacaoA,&PopulacaoB);
+    scanf("%f",&AreaemKmA,&AreaemKmB);
+    scanf("%f",&PibA,&PibB);
+    scanf("%i",&Pontos_TuristicoA,Pontos_TuristicoB);
     // Exemplo:
     // printf("Digite o código da cidade: ");
     // scanf("%s", codigo);
