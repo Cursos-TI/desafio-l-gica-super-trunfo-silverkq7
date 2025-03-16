47b651aa178f7ae406a25b55774b900a6861a5df
silverkq7 <vinico365@gmail.com>
2025-03-16 14:20:32 +0000

    finsh commit nivelnovato

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -8,7 +8,7 @@
 int main() {
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
-        char Estado ='A', Estado ='B';
+        char estado1 ='A', estado2 ='B';
         char Nome_da_CidadeA[20]="Sao Paulo",Nome_da_CidadeB[20]="Rio de janeiro";
         char codigodacidadeA[20],codigodacidadeB[20];
         int PopulacaoA , PopulacaoB ;
@@ -20,18 +20,26 @@ int main() {
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
     // utilizando a função scanf para capturar as entradas.
     // utilize o código do primeiro desafio
-    printf("Digite os dados da Cidade :\n");
-    scanf("%s",&codigodacidadeA,&codigodacidadeB);
-    scanf("%i",&PopulacaoA,&PopulacaoB);
-    scanf("%f",&AreaemKmA,&AreaemKmB);
-    scanf("%f",&PibA,&PibB);
-    scanf("%i",&Pontos_TuristicoA,Pontos_TuristicoB);
     // Exemplo:
     // printf("Digite o código da cidade: ");
     // scanf("%s", codigo);
     // 
     // (Repita para cada propriedade)
-
+    printf("Digite os dados da Cidade :\n");
+    scanf("%s",&codigodacidadeA );
+    scanf("%s",&codigodacidadeB );
+    printf("Digite a população :\n");
+    scanf("%i",&PopulacaoA );
+    scanf("%i",&PopulacaoB );
+    printf("Digite a Area porKm² :\n");
+    scanf("%f",&AreaemKmA );
+    scanf("%f",&AreaemKmB);
+    printf("Digite o Pib :\n");
+    scanf("%f",&PibA );
+    scanf("%f",&PibB );
+    printf("Digite os Pontos turistico :\n");
+    scanf("%d",&Pontos_TuristicoA );
+    scanf("%d",&Pontos_TuristicoB );
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
@@ -42,6 +50,37 @@ int main() {
     // } else {
     //     printf("Cidade 2 tem maior população.\n");
     // }
+    if (PopulacaoA > PopulacaoB)
+{
+    printf("Cidade A tem maior população.\n");
+}else
+{
+    printf("Cidade B tem maior população.\n");
+}
+if (AreaemKmA > AreaemKmB)
+{
+    printf("Cidade A tem maior Area por km².\n");
+}else
+{
+    printf("Cidade B tem maior Area por km².\n");
+
+}
+if (PibA > PibB)
+{
+    printf("Cidade A tem maior Pib.\n");
+}else
+{
+    printf("Cidade B tem maior Pib.\n");
+
+}
+if (Pontos_TuristicoA > Pontos_TuristicoB)
+{
+    printf("Cidade A tem mais Pontos turisticos.\n");
+}else
+{
+    printf("Cidade B mais Pontos turisticos.\n");
+
+}
 
     // Exibição dos Resultados:
     // Após realizar as comparações, exiba os resultados para o usuário.
@@ -49,6 +88,5 @@ int main() {
 
     // Exemplo:
     // printf("A cidade vencedora é: %s\n", cidadeVencedora);
-
     return 0;
 }
